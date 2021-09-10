8693d5f0ca3f3162c62a2f6f0080cd81771ca73f
Benoît du Garreau <benoit.dugarreaudelamechenie@thalesgroup.com>
2021-09-10 09:14:58 +0200

    Get fds from qemu

--- exec/src/main.rs
+++ exec/src/main.rs
@@ -1,13 +1,17 @@
 #![allow(clippy::fn_to_numeric_cast, clippy::unnecessary_cast)]
 
-use anyhow::{ensure, Context};
+use anyhow::{bail, ensure, Context};
 use std::{
     fs,
     io::{self, BufRead},
-    mem, ptr,
+    mem,
+    os::unix::{net::UnixListener, prelude::*},
+    ptr,
 };
 
-const LIB_PATH: &[u8] = b"/tmp/test.so\0";
+use passfd::FdPassingExt;
+
+const LIB_PATH: &[u8] = b"/usr/lib/test.so\0";
 const FUN_NAME: &[u8] = b"payload\0";
 const TOTAL_LEN: usize = LIB_PATH.len() + FUN_NAME.len();
 
@@ -98,12 +102,10 @@ impl Tracee {
     }
 
     fn peek_data(&mut self, addr: *mut libc::c_void, buf: &mut [u8]) -> io::Result<()> {
-        use std::os::unix::prelude::FileExt;
         self.mem.read_exact_at(buf, addr as _)
     }
 
     fn poke_data(&mut self, addr: *mut libc::c_void, buf: &[u8]) -> io::Result<()> {
-        use std::os::unix::prelude::FileExt;
         self.mem.write_all_at(buf, addr as _)
     }
 
@@ -147,11 +149,42 @@ impl Drop for Tracee {
     }
 }
 
+fn get_dlerror(
+    tracee: &mut Tracee,
+    regs: &mut libc::user_regs_struct,
+    dlerror: u64,
+    rip: u64,
+) -> anyhow::Result<String> {
+    // char *error = dlerror();
+    regs.rip = rip;
+    regs.rax = dlerror;
+    tracee.set_registers(&regs).context("p")?;
+    tracee.restart().context("qsd")?;
+    let res = tracee.registers().context("er")?;
+
+    let mut addr = res.rax as _;
+    let mut error = Vec::with_capacity(32);
+    let mut buf = [0];
+
+    loop {
+        tracee.peek_data(addr, &mut buf).context("os")?;
+
+        match buf {
+            [0] => break,
+            [n] => error.push(n),
+        }
+        addr = addr.wrapping_add(1);
+    }
+
+    Ok(String::from_utf8_lossy(&error).into_owned())
+}
+
 fn attach(pid: libc::pid_t, _fds: &[i32]) -> anyhow::Result<()> {
     let our_libdl = find_lib(std::process::id() as _, "libdl-2").context("our libdl")?;
     let their_libdl = find_lib(pid, "libdl-2").context("their libdl")?;
     let their_dlopen = their_libdl + (libc::dlopen as u64 - our_libdl);
     let their_dlsym = their_libdl + (libc::dlsym as u64 - our_libdl);
+    let their_dlerror = their_libdl + (libc::dlerror as u64 - our_libdl);
 
     let mut tracee = Tracee::attach(pid).context("attach")?;
 
@@ -167,6 +200,7 @@ fn attach(pid: libc::pid_t, _fds: &[i32]) -> anyhow::Result<()> {
     new_regs.r10 = (libc::MAP_PRIVATE | libc::MAP_ANONYMOUS) as _;
     new_regs.r8 = -1 as _;
     new_regs.r9 = 0;
+    new_regs.rsp = old_regs.rsp - 0x100; // Accounts for the red zone
     tracee.set_registers(&new_regs)?;
 
     let new_instrs = [
@@ -204,7 +238,16 @@ fn attach(pid: libc::pid_t, _fds: &[i32]) -> anyhow::Result<()> {
 
     new_regs = tracee.registers().context("regs3")?;
     let handle = new_regs.rax;
-    ensure!(handle != 0, "dlopen failed");
+    if handle == 0 {
+        let err = get_dlerror(&mut tracee, &mut new_regs, their_dlerror, rip as u64 + 2)
+            .context("get_dlerror")?;
+
+        tracee.poke_data(rip, &old_instrs).context("a")?;
+        tracee.set_registers(&old_regs).context("b")?;
+        tracee.continu().context("v")?;
+
+        anyhow::bail!("dlopen failed: {}", err);
+    }
 
     // dlsym(handle, FUN_NAME);
     new_regs.rip = (rip as u64) + 2;
@@ -216,7 +259,15 @@ fn attach(pid: libc::pid_t, _fds: &[i32]) -> anyhow::Result<()> {
 
     new_regs = tracee.registers().context("regs4")?;
     let payload = new_regs.rax;
-    ensure!(payload != 0, "dlsyl failed");
+    if handle == 0 {
+        let err = get_dlerror(&mut tracee, &mut new_regs, their_dlerror, rip as u64 + 2)?;
+
+        tracee.poke_data(rip, &old_instrs)?;
+        tracee.set_registers(&old_regs)?;
+        tracee.continu()?;
+
+        anyhow::bail!("dlsym failed: {}", err);
+    }
 
     // payload
     new_regs.rip = (rip as u64) + 2;
@@ -225,7 +276,10 @@ fn attach(pid: libc::pid_t, _fds: &[i32]) -> anyhow::Result<()> {
     tracee.restart().context("continue 2")?;
 
     new_regs = tracee.registers()?;
-    ensure!(new_regs.rax == 0, "payload failed");
+    if new_regs.rax != 0 {
+        let err = io::Error::from_raw_os_error(new_regs.rax as _);
+        bail!("Payload failed: {}", err);
+    }
 
     tracee.poke_data(rip, &old_instrs)?;
     tracee.set_registers(&old_regs)?;
@@ -233,9 +287,67 @@ fn attach(pid: libc::pid_t, _fds: &[i32]) -> anyhow::Result<()> {
     Ok(())
 }
 
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct kvm_regs {
+    rax: u64,
+    rbx: u64,
+    rcx: u64,
+    rdx: u64,
+    rsi: u64,
+    rdi: u64,
+    rsp: u64,
+    rbp: u64,
+    r8: u64,
+    r9: u64,
+    r10: u64,
+    r11: u64,
+    r12: u64,
+    r13: u64,
+    r14: u64,
+    r15: u64,
+    rip: u64,
+    rflags: u64,
+}
+
+const KVM_GET_REGS: u64 = 2156965505;
+
 fn main() {
     let pid = std::env::args().nth(1).expect("missing pid");
     let pid = pid.parse().unwrap();
 
+    let socket_path = "/tmp/get_fds";
+    let _ = fs::remove_file(socket_path);
+    let listener = UnixListener::bind(socket_path).context("bind").unwrap();
+    fs::set_permissions(socket_path, fs::Permissions::from_mode(0o777)).unwrap();
+
+    let handle = std::thread::spawn(move || -> anyhow::Result<_> {
+        let (socket, _) = listener.accept().context("accept")?;
+        let mut fds = [0; 3];
+
+        for fd in &mut fds {
+            *fd = socket.recv_fd()?;
+        }
+
+        Ok(fds)
+    });
+
     attach(pid, &[]).unwrap();
+
+    let fds = handle.join().unwrap().unwrap();
+
+    for fd in fds {
+        let f = mem::ManuallyDrop::new(unsafe { fs::File::from_raw_fd(fd) });
+        dbg!(f);
+    }
+    let [vm, vcpu1, vcpu2] = fds;
+
+    loop {
+        unsafe {
+            let mut regs = mem::zeroed::<kvm_regs>();
+            let res = libc::ioctl(vcpu1, KVM_GET_REGS, &mut regs as *mut _ as u64);
+            let _ = dbg!(check!(&res));
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        }
+    }
 }
--- patch/src/lib.rs
+++ patch/src/lib.rs
@@ -6,21 +6,18 @@ use std::{
 use passfd::FdPassingExt;
 
 #[no_mangle]
-pub extern "C" fn payload2() -> libc::c_int {
-    send_fds(&[15, 21, 22]).is_err() as _
+pub extern "C" fn payload() -> libc::c_int {
+    match send_fds(&[15, 21, 22]) {
+        Ok(()) => 0,
+        Err(e) => e.raw_os_error().unwrap_or(777),
+    }
 }
 
 fn send_fds(fds: &[RawFd]) -> io::Result<()> {
-    let socket = UnixStream::connect("/run/get_fds")?;
+    let socket = UnixStream::connect("/tmp/get_fds")?;
     for &fd in fds {
         socket.send_fd(fd)?;
     }
 
     Ok(())
-}
-
-#[no_mangle]
-pub extern "C" fn payload() -> libc::c_int {
-    println!("It works !");
-    0
 }
